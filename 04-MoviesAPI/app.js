b4b3f28e184963118b7666c6de7463d0ec6bd7e2
Kachimov Dmytro <137336892+Dima-Kachimov@users.noreply.github.com>
2024-04-29 00:01:55 +0300

    feat: modal window

--- 04-MoviesAPI/app.js
+++ 04-MoviesAPI/app.js
@@ -1,13 +1,16 @@
 'use strict';
+
 const form = document.querySelector('form');
 const searchInput = document.querySelector('.search');
 const logo = document.querySelector('.logo');
+const modalWindow = document.querySelector('.modal__overlay');
 
 const API_KEY = 'ceef2047-66e0-4576-9f66-941ceaf2a54b';
 const API_URL_POPULAR =
     'https://kinopoiskapiunofficial.tech/api/v2.2/films/collections?type=TOP_250_MOVIES&page=1';
 const API_URL_SEARCH =
     'https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=';
+const API_URL_MOVIE_ID = 'https://kinopoiskapiunofficial.tech/api/v2.2/films/';
 
 async function getMovies(url) {
     const res = await fetch(url, {
@@ -17,8 +20,10 @@ async function getMovies(url) {
         },
     });
     const resData = await res.json();
-    renderItemMovie(resData.items ? resData.items : resData.films);
-    console.log(resData.items, resData.films);
+    if (resData.items || resData.films) {
+        renderItemMovie(resData.items ? resData.items : resData.films);
+        console.log(resData.items, resData.films);
+    }
 }
 
 getMovies(API_URL_POPULAR).then();
@@ -30,10 +35,9 @@ function renderItemMovie(data) {
     data.forEach((movie) => {
         const rating = movie.ratingImdb ? movie.ratingImdb : movie.rating;
         const name = movie.nameRu === undefined ? movie.nameEn : movie.nameRu;
-
-        moviesWrap.insertAdjacentHTML(
-            'beforeend',
-            `
+        const id = movie.kinopoiskId ? movie.kinopoiskId : movie.filmId;
+        const moviesItem = document.createElement('div');
+        moviesItem.innerHTML = `
                 <div class="movies__item">
                     <div class="img__wrap">
                         <img
@@ -47,8 +51,11 @@ function renderItemMovie(data) {
                     <div class="movie__categories">
                         ${movie.genres.map((genre) => `  ${genre.genre}`)}
                     </div> ${rating === 'null' ? '' : `<div class="rating ration-mod__${getRatingIndicator(rating)}">${rating}</div>`}
-                </div>`
-        );
+                </div>`;
+        moviesWrap.append(moviesItem);
+        moviesItem.addEventListener('click', () => {
+            openModal(id);
+        });
     });
 }
 
@@ -65,7 +72,7 @@ function getRatingIndicator(rating) {
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     if (searchInput.value) {
-        getMovies(`${API_URL_SEARCH}${searchInput.value}`);
+        getMovies(`${API_URL_SEARCH}${searchInput.value}`).then();
         searchInput.value = '';
         searchInput.blur();
     }
@@ -74,3 +81,88 @@ form.addEventListener('submit', (e) => {
 logo.addEventListener('click', () => {
     getMovies(API_URL_POPULAR).then();
 });
+
+// MODAL
+async function openModal(id) {
+    const res = await fetch(`${API_URL_MOVIE_ID}${id}`, {
+        headers: {
+            'Content-Type': 'application/json',
+            'X-API-KEY': API_KEY,
+        },
+    });
+    const dataRes = await res.json();
+    console.log(dataRes);
+    modalWindow.classList.add('openModal');
+    modalWindow.innerHTML = `
+                <div class="modal__window">
+                    <img class="modal__close" src="./modal_close.svg" alt="" />
+                    <div class="flex__wrap">
+                        <div class="image">
+                            <img
+                                src="${dataRes.posterUrl}"
+                                alt="image"
+                            />
+                        </div>
+                        <div class="content">
+                            <table>
+                                <tr>
+                                    <th><span>Название:</span></th>
+                                    <td>
+                                        <h3 class="content__title">
+                                            ${dataRes.nameRu}
+                                        </h3>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th><span>Жанр:</span></th>
+                                    <td>
+                                        <div class="content__genres">
+                                            ${dataRes.genres.map((genre) => `  ${genre.genre}`)}
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th><span>Год выпуска:</span></th>
+                                    <td>
+                                        <div class="content__year">${dataRes.year}</div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th><span>Страна:</span></th>
+                                    <td>
+                                        <div class="content__countries">
+                                            ${dataRes.countries[0].country}
+                                        </div>
+                                    </td>
+                                </tr>
+                            </table>
+                            <div class="description">
+                                ${dataRes.description}
+                            </div>
+                            <a class="link_btn" href="${dataRes.webUrl}" target="_blank">
+                                <button class="content__webUrl">
+                                    Посмотреть на кинопоиске
+                                </button>
+                            </a>
+                        </div>
+                    </div>
+                </div>`;
+    const modalClose = document.querySelector('.modal__close');
+    const modalBody = document.querySelector('.modal__window');
+
+    modalClose.addEventListener('click', closeModal);
+    window.addEventListener('click', (e) => {
+        if (e.target === modalWindow) {
+            closeModal();
+        }
+    });
+    window.addEventListener('keydown', (e) => {
+        if (e.code === 'Escape') {
+            closeModal();
+        }
+    });
+}
+
+function closeModal() {
+    modalWindow.classList.remove('openModal');
+}
